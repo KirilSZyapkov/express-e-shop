fee3c17a36fa28c2eeb7d472eddcd791a1bfa3a4
Kiril Zyapkov <ziapkov@gmail.com>
2023-04-07 11:02:05 +0000

    Update home page display cards items.

--- controlers/detailsPage.js
+++ controlers/detailsPage.js
@@ -2,7 +2,7 @@ module.exports = async (req, res) => {
 
     const { id } = req.params;
     const item = await req.storage.getItemById(id);
-    console.log(req.session);
+    console.log(item.nutry);
     
   
     res.render("detailsPage", {
